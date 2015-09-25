ce1997301227e0e3210aa8463f1163144a881d5b
Hugues de Valon <hugues.devalon@gmail.com>
2015-09-25 17:11:20 +0200

    Première compilation

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /user/6/devalonh/Documents/sepc_tp1
+# Build directory: /user/6/devalonh/Documents/sepc_tp1/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+ADD_TEST(AllTestsAllocator "alloctest")
